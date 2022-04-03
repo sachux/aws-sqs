40d8b4933d4479a627bf318d60d549e1ad48e83e
sachux <sachinkalpetta@gmail.com>
2022-04-03 23:31:23 +0530

    Initial Commit - AWS SQS

---
+++ src/test/java/com/suchux/aws/sqs/AwsSqsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suchux.aws.sqs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsSqsApplicationTests {
+
+	//@Test
+	void contextLoads() {
+	}
+
+}
